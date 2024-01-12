0f603d513d2384489d02412c7855f955a52535f7
AmirRahman <19.amir.rahman@gmail.com>
2024-01-12 22:42:15 +0800

    added semantic search DL engine

--- semantic_search_DL/langchain_helper.py
+++ semantic_search_DL/langchain_helper.py
@@ -1,5 +1,5 @@
 from langchain.vectorstores import FAISS
-from langchain.llms import GooglePalm
+# from langchain.llms import GooglePalm
 from langchain.document_loaders.csv_loader import CSVLoader
 from langchain.embeddings import HuggingFaceInstructEmbeddings
 from langchain.prompts import PromptTemplate
@@ -10,14 +10,14 @@ from dotenv import load_dotenv
 load_dotenv()  # take environment variables from .env (especially openai api key)
 
 # Create Google Palm LLM model
-llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
+# llm = GooglePalm(google_api_key=os.environ["GOOGLE_API_KEY"], temperature=0.1)
 # # Initialize instructor embeddings using the Hugging Face model
 instructor_embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-large")
 vectordb_file_path = "faiss_index"
 
 def create_vector_db():
     # Load data from FAQ sheet
-    loader = CSVLoader(file_path='cordlife_faqs.csv', source_column="prompt")
+    loader = CSVLoader(file_path='cordlife_faqs1.csv', source_column="prompt")
     data = loader.load()
 
     # Create a FAISS instance for vector database from 'data'
@@ -28,12 +28,40 @@ def create_vector_db():
     vectordb.save_local(vectordb_file_path)
 
 
+
+
+import pandas as pd
+
+raw = pd.read_csv('cordlife_faqs1.csv')
+
+lookup = {}
+for a, b, *_ in raw.values:
+    lookup[a] = b
+
+from sentence_transformers import SentenceTransformer, util
+
+sentences = list(raw['prompt'].values)
+model = SentenceTransformer('sentence-transformers/all-MiniLM-L6-v2')
+sentences_embeddings = [model.encode(s, convert_to_tensor=True) for s in sentences]
+
+def search(q, THRESHOLD=0.65):
+    q_embedding = model.encode(q, convert_to_tensor=True)
+    out = []
+    for sentence, sentence_embedding in zip(sentences, sentences_embeddings):
+        score = util.pytorch_cos_sim(q_embedding, sentence_embedding).item()
+        out.append((sentence, score))
+    out.sort(key=lambda x:-x[-1])
+    if out[0][1] < THRESHOLD:
+        return ['I don\'t know']
+    top = out[:3]
+    return [(qns, lookup[qns], score) for qns,score in top]
+
 def get_qa_chain():
     # Load the vector database from the local folder
     vectordb = FAISS.load_local(vectordb_file_path, instructor_embeddings)
 
     # Create a retriever for querying the vector database
-    retriever = vectordb.as_retriever(score_threshold=1.0)
+    retriever = vectordb.as_retriever(score_threshold=0.7)
 
     prompt_template = """Given the following context and a question, generate an answer based on this context only.
     In the answer try to provide as much text as possible from "response" section in the source document context without making much changes.
@@ -46,15 +74,16 @@ def get_qa_chain():
     PROMPT = PromptTemplate(
         template=prompt_template, input_variables=["context", "question"]
     )
+    return search
 
-    chain = RetrievalQA.from_chain_type(llm=llm,
-                                        chain_type="stuff",
-                                        retriever=retriever,
-                                        input_key="query",
-                                        return_source_documents=True,
-                                        chain_type_kwargs={"prompt": PROMPT})
+    # chain = RetrievalQA.from_chain_type(llm=llm,
+    #                                     chain_type="stuff",
+    #                                     retriever=retriever,
+    #                                     input_key="query",
+    #                                     return_source_documents=True,
+    #                                     chain_type_kwargs={"prompt": PROMPT})
 
-    return chain
+    # return chain
 
 if __name__ == "__main__":
     create_vector_db()
---
+++ semantic_search_DL/main.py
@@ -0,0 +1,33 @@
+import streamlit as st
+from langchain_helper import get_qa_chain, create_vector_db
+
+
+
+st.title("Cordlife FAQ 🌱")
+btn = st.button("Create Knowledgebase")
+if btn:
+    create_vector_db()
+
+question = st.text_input("Question: ")
+
+def format(response):
+  try:
+    out = ''
+    for question, answer, score in response:
+        out += f'{question}\n{answer}\n(score: {score})\n\n\n'
+    return out
+  except:
+     return response[0]
+
+if question:
+    chain = get_qa_chain()
+    response = chain(question)
+
+    st.header("Answer")
+    st.write(format(response))
+
+
+
+
+
+
